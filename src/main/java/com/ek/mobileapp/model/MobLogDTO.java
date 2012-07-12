fe03ba4a32d0f82b3fdffe283d38699755a106e5
yangjiandong <young.jiandong@gmail.com>
2012-07-12 15:36:30 +0800

    add log..

---
+++ src/main/java/com/ek/mobileapp/model/MobLogDTO.java
@@ -0,0 +1,82 @@
+package com.ek.mobileapp.model;
+
+import java.io.Serializable;
+
+//用户操作mob日志,如出错异常
+public class MobLogDTO implements Serializable {
+
+    private static final long serialVersionUID = -7991345612891933364L;
+    private Long id;
+    private Long userId;
+    private String netIp;
+    private String createTime;
+    private String event;
+    private String note;
+    private String type;
+    private String from;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getNetIp() {
+        return netIp;
+    }
+
+    public void setNetIp(String netIp) {
+        this.netIp = netIp;
+    }
+
+    public String getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(String createTime) {
+        this.createTime = createTime;
+    }
+
+    public String getEvent() {
+        return event;
+    }
+
+    public void setEvent(String event) {
+        this.event = event;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+}
